a106cca6e0b46752a08fda5eb03a991a98d4c50b
BabchenkoVlad <babch.vl@gmail.com>
2022-10-13 15:55:54 +0300

    new lesson

--- src/js/циклы.js
+++ src/js/циклы.js
@@ -37,19 +37,19 @@
 // }
 
 
-// let result = '';
-// const length = 8;
+let result = '';
+const length = 8;
 
-// for (let i = 1; i < length; i++) {
+for (let i = 1; i < length; i++) {
 
-//   for (let j = 0; j < i; j++) {
-//     result += "*";
-//   }
+  for (let j = 0; j < i; j++) {
+    result += "*";
+  }
     
-//   result += '\n';
-// }
+  result += '\n';
+}
 
-// console.log(result);
+console.log(result);
 
 
 // first: for (let i = 0; i < 3; i++) {
@@ -86,19 +86,73 @@ for (let i = 2; i <= 16; i++) {
 }
 
 let i = 2;
-while (i <= 16) {
-  console.log(i);
-  i++;
-  if (i % 2 === 0) {
-    continue;
-  } else {
+
+  while (i <= 16) {
+    if (i % 2 === 0) {
+      i++;
+      continue;
+    } else {
       console.log(i);
+    }
+    i++;
   }
-}
 
 const arr = [];
 for (i = 5; i <= 10; i++) {
   arr[i - 5] = i;
 }
-  console.log(arr);
-  return arr;
+console.log(arr);
+return arr;
+
+
+
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+
+for (let i = 0; i < arr.length; i++) {
+      result[i] = arr[i];
+}
+
+console.log(result);
+return result;
+
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for (let i = 0; i < data.length; i++) {
+  if (typeof(data[i]) === 'number') {
+    data[i] = data[i] * 2;
+  } else if (typeof(data[i]) === 'string') {
+    data[i] = `${data[i]} - done`;
+  }
+}
+console.log(data);
+return data;
+
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+for (let i = 1; i <= data.length; i++) {
+  result[i - 1] = data[data.length - i];
+}
+console.log(result);
+return result;
+
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+  for (let j = 0; j < lines - i; j++) {
+    result += " ";
+  }
+  for (let j = 0; j < 2 * i + 1; j++) {
+    result += "*";
+  }
+    
+  result += '\n';
+}
+
+console.log(result);
