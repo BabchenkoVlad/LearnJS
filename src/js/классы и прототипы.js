b4ea40984fe1eaf9c67bee4c824579332f8ff2ef
BabchenkoVlad <babch.vl@gmail.com>
2022-11-08 15:10:44 +0200

    ++

--- src/js/классы и прототипы.js
+++ src/js/классы и прототипы.js
@@ -36,5 +36,38 @@ firstComment.upvote()
 console.log(firstComment.votesQty)
 
 
+//////////////////////////////////
+// ivan petrichenko
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`${this.text}, ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Hello', 'red');
+div.showMyProps();
+console.log(div.calcArea());
+
+const square = new Rectangle(10, 10);
+console.log(square.calcArea());
+
 
 
