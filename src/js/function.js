a40d938388746b494b7dfdf81c69b88fb5b35d8f
BabchenkoVlad <babch.vl@gmail.com>
2022-11-17 16:42:27 +0200

    f

--- src/js/function.js
+++ src/js/function.js
@@ -235,3 +235,42 @@ function fib(num) {
 
 console.log(fib(10));
 
+
+////////////////////////////////////////////////////
+const films = [
+    {
+        name: 'Titanic',
+        rating: 9
+    },
+    {
+        name: 'Die hard 5',
+        rating: 5
+    },
+    {
+        name: 'Matrix',
+        rating: 8
+    },
+    {
+        name: 'Some bad film',
+        rating: 4
+    }
+];
+
+function showGoodFilms(arr) {
+    
+}
+
+function showListOfFilms(arr) {
+    
+}
+
+function setFilmsIds(arr) {
+
+}
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+    
+}
+
